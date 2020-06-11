1228883f5cd3c4ce5157e87a0355ec5a62750038
lucas <lucasedux0@gmail.com>
2020-06-10 23:38:42 -0300

    logo apos formatar o pc 10/6

---
+++ estrutura_sequencial/2_numero_informado.py
@@ -0,0 +1,4 @@
+numero = int(float(input('Digite um número : ')))
+
+print('O número informado foi :',numero)
+
---
+++ estrutura_sequencial/3_dois_e_soma.py
@@ -0,0 +1,5 @@
+n1 = int(float(input('Digite o primeiro número : ')))
+n2 = int(float(input('Digite o segundo número : ')))
+
+print('A soma destes dois números é : {}'.format(n1+n2))
+
