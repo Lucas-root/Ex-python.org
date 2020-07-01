1f70685a68ba6139b25a55983ec8dc07d058e931
lucas <lucasedux0@gmail.com>
2020-07-01 18:43:46 -0300

    Finalizei o ex 17_loja_tintas2.py (O codigo ao meu ver está estavel, não está otimizado e não está limpo está sujo, dificil de compreender e não está simples

--- estrutura_sequencial/17_loja_tintas2.py
+++ estrutura_sequencial/17_loja_tintas2.py
@@ -1,11 +1,43 @@
+#By lucasedux0@gmail.com
+#in 01/07/2020
+
+# Aqui eu gravo va variavel tinta a tinta necessária
+# marco o total de latas de 3,6 e 18 litros arredondando o resto da divisão para cima
 area = float(input('\nDigite a area do local a ser pintado : '))
 tinta = area / 6
 latasP = int((tinta / 3.6)+1)
 latasG = int((tinta / 18)+1)
-# misto é 10.8 litros por 52.5 que é a media das duas latas
-misto = int((tinta / 10.8)+1)
-print('\nSerão necessários {:.1f} litros de tinta'.format(tinta))
-print('O total comprando apenas latas de 3.6 litros será de R${:.2f}'.format(latasP*25))
-print('O total comprando apenas latas de 18 litros será de R${:.2f}'.format(latasG*80))
+
+#True caso os litros de tinta necessários sejem menor do que 18 litros
+if tinta < 18:
+    print('\nO total de tinta necessário para pintar está area será de {:.1f} litros de tinta'.format(tinta))
+    print('Se você comprar latas de 3.6 litros irá precisar de {} latas o total será de R${:.2f}'.format(latasP, (latasP*25)))
+    print('Se comprar latas de 18 litros você presisará de 1 lata o total será de R$80,00')
+    print('E sobrarão {:.1f} litros de tinta.'.format(18 - tinta))
+
+else:
+    # resto quebrado da divisão de tinta/18, arredondado para cima 
+    # para então descobrir as latas de 3.6 litros necessárias 
+    restoArredondado = int(((tinta/18)-(int(tinta/18)))+1)
+    latasGr = int(tinta/18)
+
+    if (tinta%18) == 0:
+        restoArredondado -= 1
+
+    print('\nSerão necessários {:.1f} litros de tinta'.format(tinta))
+
+    print('\nSe forem comprados apenas latas de 18 litros serão necessárias {} latas de tinta'.format(latasG))
+    print('Com um total de R${:.2f}\n'.format(latasG*80))
+
+    print('Se forem compradas somente latas de 3,6 litros serão necessárias {} latas de tinta'.format(latasP))
+    print('Com um total de R${:.2f}\n'.format(latasP*25))
+
+    print('Mas se forem compradas latas mistas')
+    print('Serão necessárias {} latas de tinta de 18 litros'.format(latasGr))
+    print('Serão necessárias {} latas de tinta de 3,6 litros'.format(restoArredondado))
+    # O total será o preço das latas grandes + o preço das latas pequenas
+    total = (latasGr*80)+(restoArredondado*25)
+    print('E o total será de R${:.2f}\n'.format(total))
 
 print('\nFim do código\n')
+
